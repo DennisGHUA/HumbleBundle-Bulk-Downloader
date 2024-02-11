a0a48d34e24dcd8385305a6ae071cf6a3b164e46
Jos Badpak <31701470+DennisGHUA@users.noreply.github.com>
2024-02-12 00:34:45 +0100

    Increased delay to avoid missing downloads for OvaniSound

--- OvaniSound.js
+++ OvaniSound.js
@@ -6,6 +6,14 @@
 // Get all parent elements with the class name "sdd-checkout-item"
 var parentElements = document.getElementsByClassName("sdd-checkout-item");
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 // Function to click the second child <a> element with a delay
 function clickSecondLinkWithDelay(parentElements, index) {
     if (index < parentElements.length) {
@@ -15,14 +23,22 @@ function clickSecondLinkWithDelay(parentElements, index) {
         // Check if there is a second <a> element
         if (childLinks.length > 1) {
             // Click the second <a> element
+            console.log("Downloading (" + (index + 1) + "/" + parentElements.length + ") :" + childLinks[0].innerText);
             childLinks[1].click();
         }
         
+		var delay = getRandomArbitrary(30000, 40000);
+		
+		console.log("Waiting " + delay + " milliseconds before downloading next item...")
+		
         // Move to the next parent element after a 10-second delay
         setTimeout(function() {
             clickSecondLinkWithDelay(parentElements, index + 1);
-        }, 10000); // 10 seconds delay
-    }
+        }, delay); // xx seconds delay
+    } else {
+		console.log("Finished script.")
+		alert("All " + parentElements.length + " download buttons have been pressed you can now close this tab.")
+	}
 }
 
 // Start clicking the second <a> elements with a delay, starting from the first parent element (index 0)
